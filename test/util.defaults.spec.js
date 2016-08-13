5f60dcb132f81e1478d462b91780faa1eb15ad7b
Jordan Checkman <jordan.checkman@gmail.com>
2016-08-13 01:44:23 -0400

    defaults spec added

---
+++ test/util.defaults.spec.js
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+import { defaults } from '../src/util/util';
+
+describe('defaults', () => {
+	it('Adds missing keys', () => {
+		let objOne = {
+			foo: 1
+		};
+		let objTwo = {
+			bar: 2
+		};
+		let test = defaults(objOne, objTwo);
+		expect(test).to.have.all.keys('foo', 'bar');
+	});
+	it('Overwrites values for shared keys', () => {
+		let objOne = {
+			foo: 1,
+			bar: 2
+		};
+		let objTwo = {
+			foo: 3
+		};
+		let test = defaults(objOne, objTwo);
+		expect(test)
+			.to.have.property('foo')
+			.that.is.a('number')
+			.that.equals(3);
+	});
+});
